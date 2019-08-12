04bf6e0777322f37bd306771fbfab7eaf0d216da
Adrian Lee <adrianlee1111@gmail.com>
2019-08-12 21:53:52 +0800

    Pipfile + Output.xlsx commit

---
+++ script_trans.py
@@ -0,0 +1,63 @@
+print("""
+Batch Translate - Adrian Lee
+
+To use, simply run in command prompt. The program will ask for input. Input each
+of your desired strings, pressing enter after each string. Input blank once you
+have completed data entry. Input your desired language destination in the
+code form (eg. zh-CN, en, fr).
+
+""")
+
+from googletrans import Translator
+from pandas import ExcelWriter
+import os
+
+os.system("chcp 950")
+#This line is used so the command prompt can dispaly chinese characters.
+translator = Translator()
+
+def ask_input():
+    inputting = True
+    text = []
+    while inputting == True:
+        temp = input("Input a string: ")
+        if temp == "":
+            inputting == False
+            break
+        text.append(temp)
+    return text
+
+def ask_config(text):
+    print("\n")
+    inputting = True
+    while inputting == True:
+        try:
+            end = input("Destination Language (Two Letter Code): ")
+            translations = translator.translate(text, dest = end)
+        except:
+            print("Invalid destination language, try again.")
+            continue
+        else:
+            return translations
+
+def trans(translations):
+    print("\n" + "=============================================")
+    for translation in translations:
+        print(translation.origin, ' -> ', translation.text)
+    print("=============================================")
+
+def run():
+        text = ask_input()
+        translations = ask_config(text)
+        print("Working...")
+        trans(translations)
+
+        print("\n")
+        ask = input("Continue? (Y for yes, any other key for no.) ")
+        if ask == "Y":
+            print("\n")
+            run()
+        else:
+            print("\n Bye!")
+
+run()
